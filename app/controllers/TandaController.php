1f4fcf1371b7eacabfa332399ffdd1d1acaa2bf2
Travis Meyer <tk_meyer@aol.com>
2014-04-08 09:33:09 -0500

    Wrote TandaController skeleton

---
+++ app/controllers/TandaController.php
@@ -0,0 +1,31 @@
+<?php
+
+	class TandaController extends \BaseController {
+		public function __construct() {
+			parent::__construct();
+
+			$this->beforeFilter('auth');
+		}
+	}
+
+	public function index() {
+
+	}
+
+	public function store() {
+
+	}
+
+	public function show($id) {
+
+	}
+
+	public function edit($id) {
+		
+	}
+
+	public function destroy($id) {
+
+	}
+
+?>
