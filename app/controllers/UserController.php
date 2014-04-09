36c92c9375ca0e7c7a3edd071c99b9773690c6fd
Travis Meyer <tk_meyer@aol.com>
2014-04-09 09:54:13 -0500

    Wrote the store function for UserController

--- app/controllers/UserController.php
+++ app/controllers/UserController.php
@@ -17,6 +17,35 @@
 
 	public function store() {
 
+		$assoc = array(
+			'e1' => Input::get('email1'), 
+			'e2' => Input::get('email2'),
+			'p1' => Input::get('pass1'),
+			'p2' => Input::get('pass2'));
+
+		$newUser = new User;
+
+		if ($assoc['e1'] == $assoc['e2']) {
+			$newUser->email = $assoc['e1'];
+		} else {
+			Session::flash('loginFail', "The e-mail fields don't match");
+			return Redirect::back()->withInput();
+		}
+
+		if ($assoc['p1'] == $assoc['p2']) {
+			$newUser->password = Hash::make($assoc['p1']);
+		} else {
+			Session::flash('loginFail', "The password fields don't match");
+			return Redirect::back()->withInput();
+		}
+
+		$newUser->first_name = Input::get('first_name');
+		$newUser->last_name = Input::get('last_name');
+		$newUser->gender = Input::get('gender');
+		$newUser->DOB = Input::get('DOB');
+		$newUser->save();
+
+		return Redirect::action('DashController@show');
 	}
 
 	public function show($id) {
