fdd4331cd69c2e330a0030d903b76156c71e1d87
JaimeVelasco <jaime@photojv.com>
2014-04-09 09:42:42 -0500

    Changed position and color of signup form

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -33,38 +33,9 @@
     </section>
 
     <section id="Join" class="content-section text-center">
-        <div class="Join-section">
-            <div class="container">
-                <div class="col-lg-8 col-lg-offset-2">
-                    <h2>Join tandas</h2>
-                    <p>You can join Tandas and start your own, save for a few montsh and build your reputation so you can have access to larger Tandas, with maybe several recipients a month or something.</p>
-                    <a href="http://tandas.com" class="btn btn-default btn-lg">Visit Join Page</a>
-                </div>
-            </div>
-        </div>
-    </section>
-
-    <section id="contact" class="container content-section text-center">
-        <div class="row">
-            <div class="col-lg-8 col-lg-offset-2">
-                <h2>Contact Tandas team</h2>
-                <p>Feel free to email us to provide some feedback on our website, give us suggestions for new features or omproovements, or to just say hello!</p>
-                <p>feedback@tandas.com</p>
-                <ul class="list-inline banner-social-buttons">
-                    <li><a href="https://twitter.com/Tandas" class="btn btn-default btn-lg"><i class="fa fa-twitter fa-fw"></i> <span class="network-name">Twitter</span></a>
-                    </li>
-                    <li><a href="https://github.com/TandasCodeup/tandas.dev" class="btn btn-default btn-lg"><i class="fa fa-github fa-fw"></i> <span class="network-name">Github</span></a>
-                    </li>
-                    <li><a href="https://plus.google.com/TandasCodeup" class="btn btn-default btn-lg"><i class="fa fa-google-plus fa-fw"></i> <span class="network-name">Google+</span></a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </section>
-
-    <section id="login" class="container content-section text-center">
-     <div class="omb_login">
-    	<h3 class="omb_authTitle">Login or <a href="#">Sign up</a></h3>
+    
+     <div class="Join-section">
+    	<h3 class="omb_authTitle">Sign up <a href="#">Log in</a></h3>
 		<div class="row omb_row-sm-offset-3 omb_socialButtons">
     	    <div class="col-xs-4 col-sm-2">
 		        <a href="#" class="btn btn-lg btn-block omb_btn-facebook">
@@ -124,5 +95,24 @@
 				</p>
 			</div>
 		</div>	    	
-	</section>
+    </section>
+
+    <section id="contact" class="container content-section text-center">
+        <div class="row">
+            <div class="col-lg-8 col-lg-offset-2">
+                <h2>Contact Tandas team</h2>
+                <p>Feel free to email us to provide some feedback on our website, give us suggestions for new features or omproovements, or to just say hello!</p>
+                <p>feedback@tandas.com</p>
+                <ul class="list-inline banner-social-buttons">
+                    <li><a href="https://twitter.com/Tandas" class="btn btn-default btn-lg"><i class="fa fa-twitter fa-fw"></i> <span class="network-name">Twitter</span></a>
+                    </li>
+                    <li><a href="https://github.com/TandasCodeup/tandas.dev" class="btn btn-default btn-lg"><i class="fa fa-github fa-fw"></i> <span class="network-name">Github</span></a>
+                    </li>
+                    <li><a href="https://plus.google.com/TandasCodeup" class="btn btn-default btn-lg"><i class="fa fa-google-plus fa-fw"></i> <span class="network-name">Google+</span></a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </section>
+
 @stop
