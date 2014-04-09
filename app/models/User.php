1f27a87b703b8d07aa436e056d3811dbebcc754d
Travis Meyer <tk_meyer@aol.com>
2014-04-09 11:14:01 -0500

    Fixed User syntax errors

--- app/models/User.php
+++ app/models/User.php
@@ -19,16 +19,16 @@ class User extends BaseModel implements UserInterface, RemindableInterface {
 	 */
 	protected $hidden = array('password');
 
-	protected $guarded = ('email', 'password', 'user_role');
+	protected $guarded = array('email', 'password', 'user_role');
 
-	protected $fillable = ('email', 'first_name', 'last_name', 'gender', 'DOB');
+	protected $fillable = array('email', 'first_name', 'last_name', 'gender', 'DOB');
 
 	public function tandas() {
 		return $this->belongsToMany('Tanda');
 	}
 
 	public function isAdmin() {
-		
+
 		$role = $this->user_role;
 
 		if ($role === 1) {
