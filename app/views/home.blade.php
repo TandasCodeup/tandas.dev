c16d1a167b18a48e90f30bf38cbfd95251dbc674
JaimeVelasco <jaime@photojv.com>
2014-04-10 13:54:41 -0500

    Bye carousel, added footer.

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -6,83 +6,7 @@
         <div class="intro-body">
             <div class="container">
                 <div class="row">
-                    <!-- Carousel -->
-        <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
-            <!-- Indicators -->
-            <ol class="carousel-indicators">
-                <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-                <li data-target="#carousel-example-generic" data-slide-to="1"></li>
-                <li data-target="#carousel-example-generic" data-slide-to="2"></li>
-            </ol>
-            <!-- Wrapper for slides -->
-            <div class="carousel-inner">
-                <div class="item active">
-                    <img src="http://unsplash.s3.amazonaws.com/batch%209/barcelona-boardwalk.jpg" alt="First slide">
-                    <!-- Static Header -->
-                    <div class="header-text hidden-xs">
-                        <div class="col-md-12 text-center">
-                            <span><h1 class="brand-heading">Money Carousel</h1></span>
-                            <br>
-                            <h3>
-                                <span>What goes around comes around</span>
-                            </h3>
-                            <br>
-                            <div class="">
-                                <a class="btn btn-theme btn-sm btn-min-block" href="#">Login</a><a class="btn btn-theme btn-sm btn-min-block" href="#">Register</a></div>
-                        </div>
-                    </div><!-- /header-text -->
-                </div>
-                <div class="item">
-                    <img src="http://unsplash.s3.amazonaws.com/batch%209/barcelona-boardwalk.jpg" alt="Second slide">
-                    <!-- Static Header -->
-                    <div class="header-text hidden-xs">
-                        <div class="col-md-12 text-center">
-                            <h2>
-                                <span>Welcome to LOREM IPSUM</span>
-                            </h2>
-                            <br>
-                            <h3>
-                                <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</span>
-                            </h3>
-                            <br>
-                            <div class="">
-                                <a class="btn btn-theme btn-sm btn-min-block" href="#">Login</a><a class="btn btn-theme btn-sm btn-min-block" href="#">Register</a></div>
-                        </div>
-                    </div><!-- /header-text -->
-                </div>
-                <div class="item">
-                    <img src="http://unsplash.s3.amazonaws.com/batch%209/barcelona-boardwalk.jpg" alt="Third slide">
-                    <!-- Static Header -->
-                    <div class="header-text hidden-xs">
-                        <div class="col-md-12 text-center">
-                            <h2>
-                                <span>Welcome to LOREM IPSUM</span>
-                            </h2>
-                            <br>
-                            <h3>
-                                <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</span>
-                            </h3>
-                            <br>
-                            <div class="">
-                                <a class="btn btn-theme btn-sm btn-min-block" href="#">Login</a><a class="btn btn-theme btn-sm btn-min-block" href="#">Register</a></div>
-                        </div>
-                    </div><!-- /header-text -->
-                </div>
-            </div>
-            <!-- Controls -->
-            <a class="left carousel-control" href="#carousel-example-generic" data-slide="prev">
-                <span class="glyphicon glyphicon-chevron-left"></span>
-            </a>
-            <a class="right carousel-control" href="#carousel-example-generic" data-slide="next">
-                <span class="glyphicon glyphicon-chevron-right"></span>
-            </a>
-        </div><!-- /carousel -->
-
-
-
-
-
-                    <!-- <div class="col-md-8 col-md-offset-2">
+                    <div class="col-md-8 col-md-offset-2">
                         <h1 class="brand-heading">Money Carousel</h1>
                         <p class="intro-text">What goes around comes around</p>
                         <div class="page-scroll">
@@ -90,7 +14,7 @@
                                 <i data-icon="a"></i>
                             </a></center>
                         </div>
-                    </div> -->
+                    </div>
                 </div>
             </div>
         </div>
@@ -110,6 +34,7 @@
         <div class="col-lg-12 col-md-10 ">
             <div class="col-lg-4" >
                 <div id="circle1" class="circle"></i><h1 class="niceicons"><i data-icon="c"></i></h1>
+                    <br>
                     <p>Collaboratively administrate empowered markets via plug-and-play networks. 
                         Dynamically procrastinate B2C users after installed base benefits. 
                         Dramatically visualize customer directed convergence without revolutionary ROI.
@@ -118,6 +43,7 @@
             </div>
             <div class="col-lg-4" >    
                 <div id="circle2" class="circle"></i><h1 class="niceicons"><i data-icon="d"></i></h1>
+                   <br>
                     <p>
                         Efficiently unleash cross-media information without cross-media value. 
                         Quickly maximize timely deliverables for real-time schemas. 
@@ -127,6 +53,7 @@
             </div>
             <div class="col-lg-4" >
                 <div id="circle3" class="circle"><h1 class="niceicons"><i data-icon="b"></i></h1>
+                    <br>
                     <p>
                         Efficiently unleash cross-media information without cross-media value. 
                         Quickly maximize timely deliverables for real-time schemas. 
@@ -195,5 +122,41 @@
             </div>
         </div>
     </section>
+
+    <div id="footer">
+    <div class="container">
+        <div class="row">
+            <h3 class="footertext">Team Tanda:</h3>
+            <br>
+              <div class="col-md-4">
+                <center>
+                  <img src="http://oi60.tinypic.com/w8lycl.jpg" class="img-circle" alt="the-brains">
+                  <br>
+                  <h4 class="footertext">Programmer</h4>
+                  <p class="footertext">You can thank all the crazy programming here to this guy.<br>
+                </center>
+              </div>
+              <div class="col-md-4">
+                <center>
+                  <img src="http://oi60.tinypic.com/2z7enpc.jpg" class="img-circle" alt="...">
+                  <br>
+                  <h4 class="footertext">Artist</h4>
+                  <p class="footertext">All the images here are hand drawn by this man.<br>
+                </center>
+              </div>
+              <div class="col-md-4">
+                <center>
+                  <img src="http://oi61.tinypic.com/307n6ux.jpg" class="img-circle" alt="...">
+                  <br>
+                  <h4 class="footertext">Designer</h4>
+                  <p class="footertext">This pretty site and the copy it holds are all thanks to this guy.<br>
+                </center>
+              </div>
+            </div>
+            <div class="row">
+            <p><center><a href="#">Contact Stuff Here</a> <p class="footertext">Copyright 2014</p></center></p>
+        </div>
+    </div>
+</div>
 
 @stop
