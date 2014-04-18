76329b418198b745425bd4c6c4a752962f835539
JaimeVelasco <jaime@photojv.com>
2014-04-17 23:21:38 -0500

    Added metadata to head

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -3,8 +3,10 @@
 <head>
 	<meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="Tandas website">
-    <meta name="author" content="Codeup Team">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="description" content="Money Carousel is a community savings project">
+    <meta name="keywords" content="Savings, financial, grassroots, community, tandas, cundinas, lending circle">
+    <meta name="author" content="Travis Meyer, Mitchell Connell and Jaime Velasco">
 	 
 
 @yield('topscript')
