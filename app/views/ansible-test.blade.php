d3ba94dbc87ad912cc013035953c1d537cd3d818
Travis Meyer <tk_meyer@aol.com>
2014-07-23 17:38:28 -0500

    Can now click on test ansible button

--- app/views/ansible-test.blade.php
+++ app/views/ansible-test.blade.php
@@ -2,6 +2,12 @@
 
 @section('content')
 
-    <button class="btn btn-success btn-lg">TEST ANSIBLE</button>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6 col-md-offset-3">
+                <button class="btn btn-success btn-lg">TEST ANSIBLE</button>
+            </div>
+        </div>
+    </div>
 
 @stop
