720195669e6f54b8cc40e68fefe698c3df1ee3eb
Travis Meyer <tk_meyer@aol.com>
2014-07-23 17:40:42 -0500

    Yes

--- app/views/ansible-test.blade.php
+++ app/views/ansible-test.blade.php
@@ -2,12 +2,16 @@
 
 @section('content')
 
-    <div class="container">
-        <div class="row">
-            <div class="col-md-6 col-md-offset-3">
-                <button class="btn btn-success btn-lg">TEST ANSIBLE</button>
+    <section class="intro">
+        <div class="intro-body">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6 col-md-offset-3">
+                        <button class="btn btn-success btn-lg">TEST ANSIBLE</button>
+                    </div>
+                </div>
             </div>
         </div>
-    </div>
+    </section>
 
 @stop
